e1bc9bc248ace11f4a989213fa6c2099aba2cb22
AkashSebastian <31592003+akashsebastian333@users.noreply.github.com>
2023-03-11 14:20:34 +0530

    Delete front directory

--- front/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.indexpage, name='indexpage'),
-    #path('hello', views.job_list, name='job_list'),
-    path('jobs', views.job_list, name='job_list'),
-]
